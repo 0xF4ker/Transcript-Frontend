6c243a1c63c70ab30963a56be8edfec66661d5d8
deaththekidd <ohiemidivine7@gmail.com>
2023-09-25 04:54:17 +0100

    update

--- src/features/api/Auth/authApiSlice.ts
+++ src/features/api/Auth/authApiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApiSlice: any = createApi({
 	reducerPath: "userApiSlice",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://localhost:3001",
+		baseUrl: "https://api.transcript.dtkapp.com",
 	}),
 	endpoints: (builder) => ({
 		registerUser: builder.mutation({
