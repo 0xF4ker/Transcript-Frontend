03a77599928b58345c6d26565d9121d4d04d874c
deaththekidd <ohiemidivine7@gmail.com>
2023-09-28 02:04:09 +0100

    update

--- dist/scripts/loader.js
+++ dist/scripts/loader.js
@@ -1,6 +1,4 @@
 window.addEventListener("load", function () {
-	var load_screen = document.getElementById("load_screen");
-	document.body.removeChild(load_screen);
 	let layoutName = "Collapsible Menu";
 	let corkThemeObject;
 	let getcorkThemeObject;
